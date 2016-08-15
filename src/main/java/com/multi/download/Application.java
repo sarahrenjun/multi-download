567fa0eecc30c912b5b38e20e486e30ca19824dd
sarah <630541704@qq.com>
2016-08-15 20:43:31 +0800

    20160815

---
+++ src/main/java/com/multi/download/Application.java
@@ -0,0 +1,19 @@
+package com.multi.download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
